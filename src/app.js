3c0d1a7b92648f9b1f8ad7ee2d5a070c5a89b1fb
Cassiano Batesttin <91926273+undefined@users.noreply.github.com>
2022-02-15 02:54:00 +0000

    First commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+
+  musica = [
+    {cod:1, nome: "Linkin park", musica:"Numb", album:"Meteora"},
+    {cod:2, nome: "Coldplay", musica:"Don't Panic", album:"Parachutes"},
+    {cod:3, nome: "Queen", musica:"Under Pressure", album:"Hot Space"},
+    {cod:4, nome: "U2", musica:"Seconds", album:"War"},
+    {cod:5, nome: "System Of A Down", musica:"Prison Song", album:"Toxicity"},
+  ]
+  
+
+
+}
